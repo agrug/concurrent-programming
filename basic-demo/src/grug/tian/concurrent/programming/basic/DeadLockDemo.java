4acfccb0995a8e645acf4c89cfae46c47647671b
grug.tian <grug.tian@pkfare.com>
2018-11-11 04:46:57 +0800

    feature: 新增死锁的示例

--- basic-demo/src/grug/tian/concurrent/programming/basic/DeadLockDemo.java
+++ basic-demo/src/grug/tian/concurrent/programming/basic/DeadLockDemo.java
@@ -1,6 +1,43 @@
 package grug.tian.concurrent.programming.basic;
 
+import java.util.concurrent.TimeUnit;
+
 public class DeadLockDemo {
-    private static String A = "A";
-    private static String B = "B";
+    private static final String lockA = "A";
+    private static final String lockB = "B";
+
+    private static void deadLock() {
+        Thread thread1 = new Thread(() -> {
+            synchronized (lockA) {
+                try {
+                    TimeUnit.SECONDS.sleep(1L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (lockB) {
+                    System.out.println("thread1 finish");
+                }
+            }
+        }, "lock 1 thread");
+
+        Thread thread2 = new Thread(() -> {
+            synchronized (lockB) {
+                try {
+                    TimeUnit.SECONDS.sleep(1L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (lockA) {
+                    System.out.println("thread2 finish");
+                }
+            }
+        }, "lock 2 thread");
+
+        thread1.start();
+        thread2.start();
+    }
+
+    public static void main(String[] args) {
+        deadLock();
+    }
 }
